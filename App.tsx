9fbe724c487fc5b82323f5b28525c8d2c9329217
koshan21 <koshanrajd@srmasc.ac.in>
2026-06-27 14:53:49 +0530

    App.tsx

---
+++ App.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function App() {
+  return (
+    <div className="p-4">
+      <h1 className="text-2xl font-bold">Blog Hub</h1>
+      <p>Welcome to my Blog Management App!</p>
+    </div>
+  );
+}
+
+export default App;
